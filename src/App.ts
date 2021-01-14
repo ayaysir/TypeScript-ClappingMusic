d1246dd7e652ac7c5134f0a378550faa8aeb6241
yoonbumtae <yoonbumtae@gmail.com>
2021-01-15 01:04:40 +0900

    keypress sound

--- src/App.ts
+++ src/App.ts
@@ -1,3 +1,8 @@
+import clapHighLeft from "./assets/clap-high-left.mp3"
+import clapLowLeft from "./assets/clap-low-left.mp3"
+import clapLowRight from "./assets/clap-low-right.mp3"
+import clapHighRight from "./assets/clap-high-right.mp3"
+
 export default function App() {
     const canvas: HTMLCanvasElement = document.createElement("canvas")
     canvas.width = window.innerWidth
@@ -12,6 +17,23 @@ export default function App() {
     ctx.fillStyle = "white"
     ctx.fillText("Clapping", 30, 80)
     ctx.fillText("Music", 150, 150)
+
+    document.addEventListener("keypress", e => {
+        const keyMapping = {
+            "d": clapHighLeft,
+            "f": clapLowLeft,
+            "j": clapLowRight,
+            "k": clapHighRight
+        }
+        if(e.key === "d" || e.key === "f" || e.key === "j" || e.key === "k") {
+            const audio: HTMLAudioElement = new Audio(keyMapping[e.key])
+            audio.play()
+        }
+    })
+
+    // const img = document.createElement("img")
+    // img.src = clapHighLeft.default
+    // return img
 
     return canvas
 }
